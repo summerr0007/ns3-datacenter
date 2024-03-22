5c46324bba09242f8eaaeac98c1ea62bcdb36654
summerr0007@LAB_E113 <summerr0007@gmail.com>
2024-03-22 12:05:27 +0800

    add dcqcn NOT DONE

--- simulator/ns-3.39/examples/DCQCN/results-burst.sh
+++ simulator/ns-3.39/examples/DCQCN/results-burst.sh
@@ -8,7 +8,7 @@ mkdir $RES_RESULTS
 algs=(0 1 2 3 4 5)
 
 algNames=("dcqcn" "powerInt" "hpcc" "powerDelay" "timely" "dctcp")
-# CCMODE=(1 3 3 3 7 8)
+CCMODE=(1 3 3 3 7 8)
 
 # at the moment, power int and delay are called from hpcc ACK function separately and hence cc mode is still 3.
 
